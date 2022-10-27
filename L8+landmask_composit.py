e1928702dd0e1e5c7bd00f621b09c46257207d86
rxramir <99679528+rxramir@users.noreply.github.com>
2022-10-27 14:33:50 -0400

    Add files via upload

--- L8+landmask_composit.py
+++ L8+landmask_composit.py
@@ -20,7 +20,7 @@ def resolution_change(L8path, landmaskpath):
     tmp_comp.GetRasterBand(12).WriteArray(mask)
     
     outputpath = L8path # + '.tif'
-    dst_ds = gdal.GetDriverByName('MEM').CreateCopy(outputpath, tmp_comp, 0)
+    dst_ds = gdal.GetDriverByName("GTiff").CreateCopy(outputpath, tmp_comp, 0)
     del dst_ds
-resolution_change("C:/Users/rramirez.HBG-S1258/OneDrive - Science Systems and Applications, Inc/Documents/LC08_L1TP_002071_20181017_20181030_01_T1 (1).TIF", "C:/Users/rramirez.HBG-S1258/OneDrive - Science Systems and Applications, Inc/Documents/Land Mask Layers/LC08_L1TP_002071_20181017_20181030_01_T1/LC08_L1TP_002071_20181017_20181030_01_T1_landmask.tif")
+resolution_change("C:/Users/rramirez.HBG-S1258/OneDrive - Science Systems and Applications, Inc/Documents/September/LC08_L1TP_052014_20180727_20180731_01_T1/LC08_L1TP_052014_20180727_20180731_01_T1.TIF", "C:/Users/rramirez.HBG-S1258/OneDrive - Science Systems and Applications, Inc/Documents/Land Mask Layers/LC08_L1TP_052014_20180727_20180731_01_T1/LC08_L1TP_052014_20180727_20180731_01_T1_landmask.tif")
 #resolution_change('C:/Users/aashapur/cloud_masking/scripts/S2A_MSIL1C_20181207T160501_N0207_R054_T17PKM_20181207T192419.SAFE/IMG_DATA')
