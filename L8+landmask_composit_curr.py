b523383f35148ee84be7a1cab9b5680ca43bb5ba
rxramir <99679528+rxramir@users.noreply.github.com>
2022-11-03 18:05:41 -0400

    Add files via upload

---
+++ L8+landmask_composit_curr.py
@@ -0,0 +1,26 @@
+#import pathlib
+#import argparse
+import sys, os, glob
+import numpy as np
+from osgeo import gdal, gdalnumeric, ogr, osr
+
+#**********************************************************************************************************************
+# Code to add a land mask layer to a coposite landsat image
+# Input: path to original composited landsat image, path to land mask
+# Output: A Landsat image with the land mask added as the 12th band
+#**********************************************************************************************************************
+
+def resolution_change(L8path, landmaskpath):
+    L8_comp = gdal.Open(L8path)
+    landmask = gdal.Open(landmaskpath)
+    mask = landmask.GetRasterBand(1).ReadAsArray().astype(float)
+    
+    tmp_comp = gdal.GetDriverByName('MEM').CreateCopy('', L8_comp, 0)
+    tmp_comp.AddBand()
+    tmp_comp.GetRasterBand(12).WriteArray(mask)
+    
+    outputpath = L8path # + '.tif'
+    dst_ds = gdal.GetDriverByName("GTiff").CreateCopy(outputpath, tmp_comp, 0)
+    #del dst_ds
+resolution_change("C:/Users/rramirez.HBG-S1258/OneDrive - Science Systems and Applications, Inc/Documents/LC08_L1TP_002071_20181017_20181030_01_T1 (1).TIF", "C:/Users/rramirez.HBG-S1258/OneDrive - Science Systems and Applications, Inc/Documents/LC08_L1TP_002071_20181017_20181030_01_T1_landmask.tif")
+#resolution_change('C:/Users/aashapur/cloud_masking/scripts/S2A_MSIL1C_20181207T160501_N0207_R054_T17PKM_20181207T192419.SAFE/IMG_DATA')
